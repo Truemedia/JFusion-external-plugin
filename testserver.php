06aecf39b75cb323cd07f95d3333ab62e9a43dc5
Wade Penistone <truemedia@mediacityonline.net>
2011-07-30 00:05:21 +0100

    added server file to use in external sites as xmlrpc API

---
+++ testserver.php
@@ -0,0 +1,4 @@
+<?php
+	/* This is a test server to authenticate a JFusion site on a none JFusion site, this file may not be relevant to your
+	* requirements and if it doesn't simply remove this file */
+?>
